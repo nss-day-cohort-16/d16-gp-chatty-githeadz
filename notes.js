d5acba5c4aa506c9e8250a68428271aa53c20d28
Ben Marks <onodric@yahoo.com>
2016-10-27 14:25:08 -0500

    added notes.js
    task lists

---
+++ notes.js
@@ -0,0 +1,36 @@
+// NAV
+  // #navMain
+  // #imgLogo
+  // #txtInput
+  // #txtInput.addEventListener("keypress",function(e){if(e.which === 13){updateMessages();};});
+  // #btnClear
+  // #btnClear.addEventListener("click", clearMessages(){???});
+  // #btnClear.setAttribute("disabled",true);
+    // ulMessages.addEventListener("change", function{if this.innerHTML == ''})
+  // #navMain: position Fixed
+    // CSS overflow-y: auto;
+
+// OPTIONS
+  // #checkTheme
+    // #checkTheme.addEventListener("click", changeTheme(){???});
+  // #checkLargeText
+    // #checkLargeText.addEventListener("click", changeBodyEmValue(){???});
+
+// MESSAGES
+  // #ulMessages ==> populateIIFE(with JSONfiles)
+  // <li>msg from #txtInput.addEventListener("keypress",function(e){if(e.which === 13){updateMessages();};}); <== see above
+    // <li has a delete button (SEE DyCards)
+    // deleteBtn deletes (see above)
+
+// MODULAR CODE
+  // Augmnted Object
+    // JSONhandler
+      // XHR call
+    // AddNewMsg Augmenter
+      // addToMsgArray
+        // from JSON
+        // from #txtInput
+    // getMsgArray Augmentor
+    // deleteMsg Augmentor
+      // from #deleteBtn
+  //DomHandler (main)

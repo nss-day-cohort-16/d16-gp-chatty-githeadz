e94def7ebd723a993414fee94be0149d239b9bc4
Ben Marks <onodric@yahoo.com>
2016-10-31 09:42:12 -0500

    fixed button styling that displeased MM

--- javascripts/readerWriter.js
+++ javascripts/readerWriter.js
@@ -12,9 +12,9 @@ var Cathy = (function (oldCathy) {
   
   oldCathy.writeMsgDOM = function(elementID, msgObject, idCounter){
     var msgHTML = '';
-    msgHTML += '<li class="right clearfix" id="' + idCounter + '" style="color:' + txtPicker.value + '; background:' + bgPicker.value + '"><span class="chat-img pull-right"><img src="http://placehold.it/50/008844/fff&text=';
+    msgHTML += '<li class="right clearfix" id="' + idCounter + '" style="color:' + txtPicker.value + '; background:' + bgPicker.value + '"><span class="chat-img pull-left"><img src="http://placehold.it/50/008844/fff&text=';
     msgHTML += msgObject.user.slice(0, 3);
-    msgHTML += '" alt="User Avatar" class="img-circle"><button class="btn btn-default" type="button" style="color:' + txtPicker.value + '; background:' + bgPicker.value + '">Delete</button></span><div class="chat-body clearfix"><div class="header"><small><span class="glyphicon glyphicon-time"></span>';
+    msgHTML += '" alt="User Avatar" class="img-circle"></span><span class="chat-img pull-right"><button class="btn btn-default" type="button" style="color:' + txtPicker.value + '; background:' + bgPicker.value + '">Delete</button></span><div class="chat-body clearfix"><div class="header"><small><span class="glyphicon glyphicon-time"></span>';
     msgHTML += msgObject.timestamp;
     msgHTML += '</small><p class="pull-right primary-font">';
     msgHTML += msgObject.user;

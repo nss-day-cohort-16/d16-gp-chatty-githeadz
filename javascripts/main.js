c8720680a293c690fa94dafe65e1a700c6309567
Samuel Phillips <samphillips1879@gmail.com>
2016-11-08 08:53:43 -0600

    jQuery implementation complete
    edit modal functioning

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,9 +2,8 @@
 
 let ulMessages = $("#ulMessages"),
     txtInput = $("#txtInput"),
-
     editInput = $("#editInput"),
-
+    editSave = $("#editSave"),
     btnClear = $("#btnClear"),
     btnLargeText = $("#btnLargeText"),
     btnColor = $("#btnColor"),
@@ -14,6 +13,7 @@ let ulMessages = $("#ulMessages"),
     $userName = $("#userName"),
     editToggle = false,
     editMessage;
+    
 function initMsg(arrayOfMsgs){
   arrayOfMsgs.messages.forEach(function(message, index) {
     Cathy.writeMsgArray(message);
@@ -21,49 +21,7 @@ function initMsg(arrayOfMsgs){
   });
 }
 
-
-
-
-
-//modal edition inputHandler
-
-// function inputHandler(event) {
-//   //I'd like to change this so that it opens a new "edit" window for the user, with a text input box that is pre-populated with the contents of editMessage, instead of focusing on the main message input and starting with a blank value.
-//   if (!editToggle){
-//     if (event.which === 13){
-//       if(txtInput.val() === ''){
-//         alert("Type Something....");
-//       } else {
-//       let indexNum = Cathy.getMsgArray().length,
-//       dateNow = new Date(Date.now()),
-//       userName = document.getElementById("userName").value;
-//       if(userName === ''){userName = 'Guest';}
-//       let msgObject = { "user": userName, "timestamp": dateNow, "message": txtInput.val()};
-//       Cathy.writeMsgDOM(ulMessages, msgObject, indexNum);
-//       Cathy.writeMsgArray(msgObject);
-//       txtInput.val('');
-//       }
-//     }
-//   } else {
-//     if (event.which === 13){
-//       if(editInput.val() === ''){
-//         alert("Type Something....");
-//       } else {
-//         editMessage.html(editInput.val());
-//         editToggle = false;
-//         editInput.val('');
-//       }
-//     }
-//   }
-// }
-
-
-
-
-//regular inputHandler
-
 function inputHandler(event) {
-  //I'd like to change this so that it opens a new "edit" window for the user, with a text input box that is pre-populated with the contents of editMessage, instead of focusing on the main message input and starting with a blank value.
   if (!editToggle){
     if (event.which === 13){
       if(txtInput.val() === ''){
@@ -81,50 +39,16 @@ function inputHandler(event) {
     }
   } else {
     if (event.which === 13){
-      if(txtInput.val() === ''){
+      if(editInput.val() === ''){
         alert("Type Something....");
       } else {
-        editMessage.html(txtInput.val());
+        editMessage.html(editInput.val());
         editToggle = false;
-        txtInput.val('');
+        editInput.val('');
       }
     }
   }
 }
-
-
-
-
-//modal
-
-// $(document).on("click", "#ulMessages", function(event) {
-//   let target = $(event.target);
-//   if (target.html() === 'Delete'){
-//     let msgElement = target.parents(".right");
-//     Cathy.removeMsg(msgElement);
-//     footerMain.html('<span class="pull-left">&copy; gitHeadz 2016. ****Sam Phillips jQuery Edition****</span><span class="pull-right">Number of Messages: ' + Cathy.getMsgArray().length + '</span>');
-//   } else if (target.html() == "Edit"){
-//     let $messageContents = $(event.target).parents("li").find("strong");
-//     editToggle = true;
-//     editInput.val($messageContents.html());
-//     editMessage = $messageContents;
-//     editInput.focus();
-//   }
-// });
-
-// txtInput.keyup(inputHandler);
-// $userName.keyup(inputHandler);
-// editInput.keyup(inputHandler);
-
-// $("#editModal").on('shown.bs.modal', function () {
-//   editInput.focus();
-// });
-
-
-
-
-
-//regular
 
 $(document).on("click", "#ulMessages", function(event) {
   let target = $(event.target);
@@ -132,22 +56,30 @@ $(document).on("click", "#ulMessages", function(event) {
     let msgElement = target.parents(".right");
     Cathy.removeMsg(msgElement);
     footerMain.html('<span class="pull-left">&copy; gitHeadz 2016. ****Sam Phillips jQuery Edition****</span><span class="pull-right">Number of Messages: ' + Cathy.getMsgArray().length + '</span>');
-  } else if ($(event.target).html() == "Edit"){
+  } else if (target.html() == "Edit"){
     let $messageContents = $(event.target).parents("li").find("strong");
     editToggle = true;
-    txtInput.focus();
-    txtInput.val($messageContents.html());
+    editInput.val($messageContents.html());
     editMessage = $messageContents;
+    editInput.focus();
   }
 });
 
 txtInput.keyup(inputHandler);
 $userName.keyup(inputHandler);
+editInput.keyup(inputHandler);
+editSave.click(inputHandler);
 
 
+$("#editModal").on('shown.bs.modal', function () {
+  editInput.focus();
+});
 
-
-
+$("#editModal").on("click", "#editSave", function(event) {
+  editMessage.html(editInput.val());
+  editToggle = false;
+  editInput.val('');
+});
 
 btnClear.click(function () {
   btnClear.disabled = true;
@@ -161,14 +93,6 @@ btnLargeText.click(function(){
   $("input").toggleClass("btn-embiggin");
 });
 
-
-
-
-
-
-
-
-
 btnColor.click(function(){
   let $modal = $("#myModal").find(".modal-content"),
   $editModal = $("#editModal").find(".modal-content"),
@@ -181,23 +105,13 @@ btnColor.click(function(){
   
   $modal.css("color", txtPicker.val());
   $modal.css("background", bgPicker.val());
-
   $editModal.css("color", txtPicker.val());
   $editModal.css("background", bgPicker.val());
-
-
   $body.css("background-color", txtPicker.val());
   $messages.css("color", txtPicker.val());             
   $messages.css("background", bgPicker.val());
-
-  // $heading.css("color", txtPicker.val());
-  // $heading.css("background", bgPicker.val());
-  // $footer.css("color", txtPicker.val());
-  // $footer.css("background", bgPicker.val());
-  
   $ulMessages.parent().css("color", txtPicker.val());
   $ulMessages.parent().css("background", bgPicker.val());
-
   
   for (let i = 0; i < $tempBtns.length; i++){
     $tempBtns.eq(i).css("color", txtPicker.val());
@@ -205,7 +119,6 @@ btnColor.click(function(){
   }
   
   let $tempLis = $("li");
-  
   for(let i = 0; i < $tempLis.length; i++){
     $tempLis.eq(i).css("color", txtPicker.val());
     $tempLis.eq(i).css("background", bgPicker.val());
